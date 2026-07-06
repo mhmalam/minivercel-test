c01ec6dcf14ef4fd2659cefbb4abde29b2c4dafd
Mohammed <62259841+mhmalam@users.noreply.github.com>
2026-07-05 20:02:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/plain" });
-    res.end("its working!!\n");
+    res.end("working!123\n");
   }).listen(port, () => console.log(`listening on ${port}`));
