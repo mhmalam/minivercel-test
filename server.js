4faba904431f3a29e07fed89d24ceecf954bbf6a
Mohammed <62259841+mhmalam@users.noreply.github.com>
2026-07-05 20:11:55 -0400

    Change response message to indicate auto-build success

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/plain" });
-    res.end("working!123\n");
+    res.end("Testing auto-build ~ WORKED\n");
   }).listen(port, () => console.log(`listening on ${port}`));
