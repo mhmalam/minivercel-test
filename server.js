490ba1a63f49d99d625346f46df0ac6201246f5f
Mohammed <62259841+mhmalam@users.noreply.github.com>
2026-07-05 20:50:41 -0400

    Update response message for auto-build testing

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Testing auto-build: Delay probably 1-2 minutes\n");
+    res.end("Testing auto-builds\n");
   }).listen(port, () => console.log(`listening on ${port}`));
